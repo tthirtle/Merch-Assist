95d09a7ad6f9a093bd81f4a80cc2cf5e4fb0e68a
tthirtle <tthirtle@me.com>
2025-05-29 18:56:14 -0500

    Update _global.py

--- src/_global.py
+++ src/_global.py
@@ -1,5 +1,11 @@
 # OOP classes
 class item:
     def __init__(self,upc="",sku="",brand="",desc="",status="",pack_size="",unit_size="",img="") -> None:
-        pass
-    
+        self.upc = upc
+        self.sku = sku
+        self.brand = brand
+        self.desc = desc
+        self.status = status
+        self.pack_size = pack_size
+        self.unit_size = unit_size
+        self.img = img
